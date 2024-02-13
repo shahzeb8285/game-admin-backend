22fb48ba55e29e29e652df231c3f22ec977c60f0
shahzeb8285 <shahzeb8285@gmail.com>
2024-02-13 19:57:13 +0530

    bugs fixed

--- src/finances/finances.module.ts
+++ src/finances/finances.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FinancesService } from './finances.service';
 import { FinancesResolver } from './finances.resolver';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [FinancesResolver, FinancesService]
+  imports: [HttpModule,],
+
+  providers: [FinancesResolver, FinancesService,ConfigService]
 })
 export class FinancesModule {}
